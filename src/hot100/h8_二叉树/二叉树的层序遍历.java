46fa1e25a409eb3a0461646d1ae05eb1082e7436
Cuichenglong <ccl12538@163.com>
2024-01-16 23:45:30 +0800

    hot

--- src/hot100/h8_二叉树/二叉树的层序遍历.java
+++ src/hot100/h8_二叉树/二叉树的层序遍历.java
@@ -12,17 +12,46 @@ import java.util.Queue;
  */
 public class 二叉树的层序遍历 {
 
-//    public List<List<Integer>> levelOrder(TreeNode root) {
-//        List<List<Integer>> res = new ArrayList<>();
-//        if (root == null) return res;
-//        Queue<TreeNode> queue = new LinkedList<>();
-//        queue.offer(root);
-//        while (!queue.isEmpty()) {
-//            List<Integer> temp = new ArrayList<>();
-//            TreeNode poll = queue.poll();
-//            temp.add(poll.val);
-//        }
-//
-//    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode poll = queue.poll();
+                temp.add(poll.val);
+                if (poll.left != null) queue.offer(poll.left);
+                if (poll.right != null) queue.offer(poll.right);
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+
+
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root != null) queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+//    作者：Krahets
+//    链接：https://leetcode.cn/problems/binary-tree-level-order-traversal/solutions/2361604/102-er-cha-shu-de-ceng-xu-bian-li-yan-du-dyf7/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 }

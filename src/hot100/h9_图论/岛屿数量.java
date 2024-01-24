56e673073b577ab7d50013e8eeb2599e846531ac
Cuichenglong <ccl12538@163.com>
2024-01-24 23:57:35 +0800

    hot

---
+++ src/hot100/h9_图论/岛屿数量.java
@@ -0,0 +1,37 @@
+package hot100.h9_图论;
+
+/**
+ * @Author CuiChengLong
+ * @Date 2024/1/24 23:52
+ * @Description
+ */
+
+public class 岛屿数量 {
+
+    int res = 0;
+    public int numIslands(char[][] grid) {
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        if (grid[i][j] == 1) {
+            grid[i][j] = '0';
+        }
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+    }
+}
+
+}

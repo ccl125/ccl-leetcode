7008f01624ad6218576c47f329b07495c4ec9313
Cuichenglong <ccl12538@163.com>
2024-01-24 23:59:29 +0800

    hot

--- src/hot100/h9_图论/岛屿数量.java
+++ src/hot100/h9_图论/岛屿数量.java
@@ -21,17 +21,14 @@ public class 岛屿数量 {
         return res;
     }
     public void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
             return;
         }
-        if (grid[i][j] == 1) {
-            grid[i][j] = '0';
-        }
+        grid[i][j] = '0';
         dfs(grid, i, j + 1);
         dfs(grid, i, j - 1);
         dfs(grid, i - 1, j);
         dfs(grid, i + 1, j);
     }
-}
 
 }

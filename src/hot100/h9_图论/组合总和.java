024feed0e2ff3a2ae679888a084ea76f46a4eb93
Cuichenglong <ccl12538@163.com>
2024-01-29 23:39:09 +0800

    hot

---
+++ src/hot100/h9_图论/组合总和.java
@@ -0,0 +1,33 @@
+package hot100.h9_图论;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author CuiChengLong
+ * @Date 2024/1/27 19:50
+ * @Description
+ */
+
+public class 组合总和 {
+
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> temp = new ArrayList<>();
+        dfs(candidates, temp, target, 0);
+        return res;
+    }
+
+    public void dfs(int[] candidates, List<Integer> temp, int target, int index) {
+        if (target < 0) return;
+        if (target == 0) {
+            res.add(new ArrayList<>(temp));
+        }
+        for (int i = index; i < candidates.length; i ++) {
+            temp.add(candidates[index]);
+            dfs(candidates, temp, target - candidates[index], index);
+            temp.remove();
+        }
+    }
+
+}
